981e36d61d6094fecb22dc58480d6fb3ccf674fe
Yuzhu Jiang <yuzhujiang0324@163.com>
2023-03-19 19:22:54 +0800

    add rm and exception info output

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Wordlist
+
+## 运行说明
+
+- bin/Wordlist.exe
+
+进入命令行输窗口入命令，如：
+> ./Wordlist.exe -r -h a -j b -w ./input.txt
+
+若有异常，提示信息将输出到命令行窗口；若无异常，则计算结果输出到该目录下的 solution.txt 中。
+
+- guibin/WordChainQt5.exe
+
+双击运行，按窗口提示操作即可。
--- main.cpp
+++ main.cpp
@@ -4,6 +4,44 @@ int main(int argc, char **argv) {
     auto controller = new Controller();
     int ret;
     int code = controller->Cmd(argc, argv, &ret);
+    switch (code) {
+        case kNoFilePath:
+            std::cout << "No File Path" << std::endl;
+            break;
+        case kMultiFilePath:
+            std::cout << "Multiple File Path" << std::endl;
+            break;
+        case kFileNotExists:
+            std::cout << "File Not Exists" << std::endl;
+            break;
+        case kFileTypeError:
+            std::cout << "File Type Error" << std::endl;
+            break;
+        case kIllegalParam:
+            std::cout << "Illegal Parameter" << std::endl;
+            break;
+        case kNoFunctionalParam:
+            std::cout << "No Functional Parameter" << std::endl;
+            break;
+        case kParamsConflict:
+            std::cout << "Parameters Conflict" << std::endl;
+            break;
+        case kDuplicateParam:
+            std::cout << "Duplicate Parameter" << std::endl;
+            break;
+        case kCharNotAssign:
+            std::cout << "Char Not Assign" << std::endl;
+            break;
+        case kIllegalChar:
+            std::cout << "Illegal Char" << std::endl;
+            break;
+        case kUnexpectedLoop:
+            std::cout << "Unexpected Loop" << std::endl;
+            break;
+        case kLengthOverflow:
+            std::cout << "Length Overflow" << std::endl;
+            break;
+    }
 //    std::cout << "return code: " << code << std::endl;
     return 0;
 }
